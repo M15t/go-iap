1aa3b5eebaf3ef65b78a260e73abf0e92a86878b
Nguyen Nguyen <nguyen@buuuk.com>
2019-03-28 11:22:23 +0700

    Add more method to cover data on response

--- playstore/model.go
+++ playstore/model.go
@@ -64,3 +64,39 @@ func (r IABResponse) IsExpired() bool {
 		return r.SubscriptionPurchase.ExpiryTimeMillis < now
 	}
 }
+
+// IsTrial checks if the subscription is trial or not
+func (r IABResponse) IsTrial() bool {
+	switch {
+	case !r.IsValidSubscription() && r.SubscriptionPurchase.PaymentState != 2:
+		return false
+	default:
+		return true
+	}
+}
+
+// GetPaymentState cover int to string for PaymentState
+func (r IABResponse) GetPaymentState() string {
+	state := r.SubscriptionPurchase.PaymentState
+	switch {
+	case state == 0:
+		return "PAYMENT_PENDING"
+	case state == 1:
+		return "PAYMENT_RECEIVED"
+	case state == 2:
+		return "FREE_TRIAL"
+	default:
+		return "PENDING_DEFERRED_UPGRADE_DOWNGRADE"
+	}
+}
+
+// GetPurchaseType cover int to string for PurchaseType
+func (r IABResponse) GetPurchaseType() string {
+	t := r.SubscriptionPurchase.PurchaseType
+	switch {
+	case *t == 0:
+		return "TEST"
+	default:
+		return ""
+	}
+}
